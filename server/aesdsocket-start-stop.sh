7f694420033216bec7c5af69b97445781f4e20fb
yusufabdulsttar <yusufabdulsttar@gmail.com>
2023-11-20 14:14:02 +0200

    add aesdsocket-start-stop for assignment 5 part 2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#--------------------------------------
+# Author: Yusuf Abdulsttar
+#--------------------------------------
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a aesdsocket -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
